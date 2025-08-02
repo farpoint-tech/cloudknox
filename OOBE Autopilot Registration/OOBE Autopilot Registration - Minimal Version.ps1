a80e441f63524a4de26d75b8877a8a8caf4540ad
farpoint-tech <ps@farpoint.tech>
2025-08-02 15:43:34 +0200

    Update OOBE Autopilot Registration - Minimal Version.ps1

--- OOBE Autopilot Registration/OOBE Autopilot Registration - Minimal Version.ps1
+++ OOBE Autopilot Registration/OOBE Autopilot Registration - Minimal Version.ps1
@@ -1,1 +1,2 @@
 
+Set-ExecutionPolicy Bypass -Force [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 Install-Script Get-WindowsAutoPilotInfo -Force -Scope CurrentUser Get-WindowsAutoPilotInfo -GroupTag "userdriven" -Online Write-Host "SUCCESS: Autopilot Registration completed!" -ForegroundColor Green
